f037aa437b2075122c851eeb9a924166f27b9028
asadilyas404 <asadilyas404@gmail.com>
2025-06-12 11:55:50 +0500

    asd

--- resources/views/vendor-views/order/order-view.blade.php
+++ resources/views/vendor-views/order/order-view.blade.php
@@ -313,7 +313,7 @@
                                         @if (isset($detail->food_id))
                                             @php($detail->food = json_decode($detail->food_details, true))
                                             @php($food = \App\Models\Food::where(['id' => $detail->food['id']])->first())
-                                            @php(dd(\App\Models\Translation::where('translationable_id', $detail->food['id'])->get()));
+                                            {{-- @php(dd(\App\Models\Translation::where('translationable_id', $detail->food['id'])->get())); --}}
                                             <tr>
                                                 <td>
                                                     <div class="media">
@@ -336,6 +336,7 @@
                                                                                 ->where('locale', 'ar')
                                                                                 ->first()->value ??
                                                                             $detail->food['name'];
+                                                                        dd($arabicName);
                                                                     @endphp
 
                                                                     <strong>{{ Str::limit($arabicName, 25, '...') }}</strong><br>

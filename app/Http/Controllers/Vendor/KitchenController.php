decf08cadcec4f0a88f899a94b7bad6f97a8cb11
asadilyas404 <asadilyas404@gmail.com>
2025-06-17 15:23:05 +0500

    asd

--- app/Http/Controllers/Vendor/KitchenController.php
+++ app/Http/Controllers/Vendor/KitchenController.php
@@ -77,15 +77,15 @@ class KitchenController extends Controller{
 
             $order = Order::where('id', $request->id)->first();
             if($order) {
-                $order->kitchen_status = $request->type;
-                $order->is_pushed = 'N';
-                $order->save();
-
                 KitchenOrderStatusLog::create([
                     "status" => $request->type,
                     "order_id" => $order->id,
                     "global_id" => $order->global_id,
                 ]);
+
+                $order->kitchen_status = $request->type;
+                $order->is_pushed = 'N';
+                $order->save();
             }
         }
 

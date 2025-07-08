347a42814d97a09bfb1738b6d98d6a1b1fc432f1
asadilyas404 <asadilyas404@gmail.com>
2025-07-08 13:29:15 +0500

    asd

--- resources/views/vendor-views/order/list.blade.php
+++ resources/views/vendor-views/order/list.blade.php
@@ -480,7 +480,7 @@
             @else
                 <div class="row">
                     @foreach ($orders as $order)
-                        <div class="col-md-6 col-xl-4 p-3">
+                        <div class="col-md-6 col-xl-4 p-2">
                             <div class="card border order-card h-100 shadow-sm">
                                 <div class="card-body p-3 pb-2">
                                     <!-- Header: Order # and Status -->

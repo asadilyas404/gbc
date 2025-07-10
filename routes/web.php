6f7867574a2d53e472b079864a4d19b753f68b15
asadilyas404 <asadilyas404@gmail.com>
2025-07-10 12:10:22 +0500

    asd

--- routes/web.php
+++ routes/web.php
@@ -245,7 +245,7 @@ Route::group(['prefix' => 'deliveryman', 'as' => 'deliveryman.'], function () {
 });
 
 
-Route::post('/qz/sign', function (\Illuminate\Http\Request $request) {
+Route::get('/qz/sign', function (\Illuminate\Http\Request $request) {
     $data = $request->input('data');
 
     $privateKeyPath = storage_path('app/keys/key.pem');

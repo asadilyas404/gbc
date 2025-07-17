f4e87dbafd73b46310b4485c65366b60df22857b
asadilyas404 <asadilyas404@gmail.com>
2025-07-17 13:53:05 +0500

    asd

--- resources/views/new_invoice.blade.php
+++ resources/views/new_invoice.blade.php
@@ -29,7 +29,7 @@
                     {{-- <h5 class="initial-38-3">{{ $order->restaurant->name }} | مالك البيتزا </h5> --}}
                     <h5 class="initial-38-3">{{ $order->restaurant->name }} | {{ config('invoice.restaurant_name') }} </h5>
                     <h5 class="text-break initial-38-4">
-                        {{ $order->restaurant->address }} | {{ env('INVOICE_RESTAURANT_NAME') }}
+                        {{ $order->restaurant->address }} | {{ env('invoice.branch_name') }}
                     </h5>
                     {{-- <h5 class="text-break initial-38-4">
                         المصنعة

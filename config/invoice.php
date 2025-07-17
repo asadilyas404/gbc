ea11a0739252919098dcba5085ee5503e2680960
asadilyas404 <asadilyas404@gmail.com>
2025-07-17 13:45:01 +0500

    asd

--- config/invoice.php
+++ config/invoice.php
@@ -1,5 +1,6 @@
+<?php
 return [
-    'restaurant_name' => env('INVOICE_RESTAURANT_NAME'ن),
+    'restaurant_name' => env('INVOICE_RESTAURANT_NAME'),
     'branch_name' => env('INVOICE_BRANCH_NAME'),
     'slogan' => env('INVOICE_SLOGAN'),
 ];

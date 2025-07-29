44a705929b8c03c99863c5e8891a99fcb88b8a03
asadilyas404 <asadilyas404@gmail.com>
2025-07-29 13:01:36 +0500

    asd

--- app/Http/Controllers/Vendor/CategoryController.php
+++ app/Http/Controllers/Vendor/CategoryController.php
@@ -27,7 +27,6 @@ class CategoryController extends Controller
 
             ->paginate(config('default_pagination'));
 
-        dd($categories);
 
         return view('vendor-views.category.index', compact('categories'));
     }
@@ -58,6 +57,8 @@ class CategoryController extends Controller
 
     function store(Request $request)
     {
+        $maxTranslationId = DB::table('translations')->max('id') ?? 0;
+        dd($maxTranslationId);
         $request->validate([
             'name' => 'required|unique:categories|max:100',
             'image' => 'nullable|max:2048',

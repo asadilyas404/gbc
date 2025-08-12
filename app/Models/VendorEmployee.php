0361f0b234892d2d661858c5237c50062d9237e6
asadilyas404 <asadilyas404@gmail.com>
2025-08-12 14:55:30 +0500

    asd

--- app/Models/VendorEmployee.php
+++ app/Models/VendorEmployee.php
@@ -13,7 +13,7 @@ class VendorEmployee extends Authenticatable
 
     public $incrementing = false;
     protected $primaryKey = 'id';
-    protected $table = 'users';
+    // protected $table = 'users';
 
     protected $hidden = [
         'password',

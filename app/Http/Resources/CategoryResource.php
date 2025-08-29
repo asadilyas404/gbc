40f822c0e4a12726ed58834298398d38c6c806ee
asadilyas404 <asadilyas404@gmail.com>
2025-08-29 12:56:39 +0500

    asd

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -9,7 +9,7 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
+            'id' => (int) $this->id,
             'name' => $this->name,
             'foods' => FoodResource::collection($this->foods) // Use relationship directly
         ];
--- app/Http/Resources/FoodResource.php
+++ app/Http/Resources/FoodResource.php
@@ -9,71 +9,71 @@ class FoodResource extends JsonResource
     public function toArray($request)
     {
         $originalPrice = $this->price;
-$discount = $this->discount ?? 0;
-$discountType = $this->discount_type ?? 'percent'; // 'percent' or 'amount'
+        $discount = $this->discount ?? 0;
+        $discountType = $this->discount_type ?? 'percent'; // 'percent' or 'amount'
 
-$discountAmount = 0;
+        $discountAmount = 0;
 
-if ($discount > 0) {
-    if ($discountType === 'percent') {
-        $discountAmount = ($originalPrice * $discount) / 100;
-    } else {
-        $discountAmount = $discount;
-    }
+        if ($discount > 0) {
+            if ($discountType === 'percent') {
+                $discountAmount = ($originalPrice * $discount) / 100;
+            } else {
+                $discountAmount = $discount;
+            }
 
-    $discountedPrice = max(0, $originalPrice - $discountAmount);
-    $price = round($discountedPrice, 2);
-} else {
-    $price = round($originalPrice, 2);
-    $discountType = 'percent'; // default value when no discount
-}
+            $discountedPrice = max(0, $originalPrice - $discountAmount);
+            $price = round($discountedPrice, 2);
+        } else {
+            $price = round($originalPrice, 2);
+            $discountType = 'percent'; // default value when no discount
+        }
 
-$data = [
-    'id' => $this->id,
-    'name' => $this->name,
-    'image' => url('storage/product/' . $this->image),
-    'price' => $price,
-'discount_price' => (double) round($discountAmount, 2),
-    'discount_type' => $discountType,
-    'available_time_starts' => $this->available_time_starts,
-    'available_time_ends' => $this->available_time_ends,
-    'addons' => AddOnResource::collection($this->getAddOns()),
-];
-
-        
-        
-
-if ($this->variationOptions && $this->variationOptions->count() > 0) {
-    $data['variations'] = [];
-
-    // Group variationOptions by variation_id
-    $grouped = $this->variationOptions->groupBy('variation_id');
-
-    foreach ($grouped as $variationId => $options) {
-        $variation = optional($options->first()->variation); // Get the Variation model
-
-        $data['variations'][] = [
-            'heading' => $variation->name ?? 'Unnamed',
-            'type' => $variation->type ?? 'multi',
-            'min' => $variation->type === 'multi' ? $variation->min : null,
-            'max' => $variation->type === 'multi' ? $variation->max : null,
-            'is_required' => $variation->is_required ?? false,
-            'data' => $options->map(function ($option) {
-                return [
-                    'option_name' => $option->option_name,
-                    'option_price' => $option->option_price,
-                ];
-            })->values(),
+        $data = [
+            'id' => (int) $this->id,
+            'name' => $this->name,
+            'image' => url('storage/product/' . $this->image),
+            'price' => $price,
+            'discount_price' => (double) round($discountAmount, 2),
+            'discount_type' => $discountType,
+            'available_time_starts' => $this->available_time_starts,
+            'available_time_ends' => $this->available_time_ends,
+            'addons' => AddOnResource::collection($this->getAddOns()),
         ];
-    }
-}
 
 
 
 
-        
+        if ($this->variationOptions && $this->variationOptions->count() > 0) {
+            $data['variations'] = [];
+
+            // Group variationOptions by variation_id
+            $grouped = $this->variationOptions->groupBy('variation_id');
+
+            foreach ($grouped as $variationId => $options) {
+                $variation = optional($options->first()->variation); // Get the Variation model
+
+                $data['variations'][] = [
+                    'heading' => $variation->name ?? 'Unnamed',
+                    'type' => $variation->type ?? 'multi',
+                    'min' => $variation->type === 'multi' ? $variation->min : null,
+                    'max' => $variation->type === 'multi' ? $variation->max : null,
+                    'is_required' => $variation->is_required ?? false,
+                    'data' => $options->map(function ($option) {
+                        return [
+                            'option_name' => $option->option_name,
+                            'option_price' => $option->option_price,
+                        ];
+                    })->values(),
+                ];
+            }
+        }
+
+
+
+
+
         return $data;
-        
+
     }
 
     private function getAddOns()
@@ -83,7 +83,7 @@ if ($this->variationOptions && $this->variationOptions->count() > 0) {
             if (is_array($ids)) {
                 // Ensure integer and unique IDs
                 $uniqueIds = array_unique(array_map('intval', $ids));
-    
+
                 // Fetch only once, then ensure unique collection by ID
                 return \App\Models\AddOn::whereIn('id', $uniqueIds)
                     ->get()
@@ -91,7 +91,7 @@ if ($this->variationOptions && $this->variationOptions->count() > 0) {
                     ->values(); // reindex to avoid gaps
             }
         }
-    
+
         return collect();
     }
 }
@@ -102,47 +102,47 @@ if ($this->variationOptions && $this->variationOptions->count() > 0) {
 
 
 
-        // $originalPrice = $this->price;
-        // $discount = $this->discount ?? 0;
-        // $discountType = $this->discount_type ?? 'percent'; // assumes 'percent' or 'amount'
-        
-        // if ($discount > 0) {
-        //     if ($discountType === 'percent') {
-        //         $discountedPrice = $originalPrice - ($originalPrice * $discount / 100);
-        //     } else { // fixed amount
-        //         $discountedPrice = $originalPrice - $discount;
-        //     }
-        
-        //     // Ensure price doesn't go negative
-        //     $discountedPrice = max(0, $discountedPrice);
-        
-        //     $data = [
-        //         'id' => $this->id,
-        //         'name' => $this->name,
-        //         'image' => url('storage/product/' . $this->image),
-        //         'price' => round($discountedPrice, 2),
-        //         'discount_price' => round($originalPrice, 2),
-        //         'addons' => AddOnResource::collection($this->getAddOns()),
-        //     ];
-        // } else {
-        //     // No discount
-        //     $data = [
-        //         'id' => $this->id,
-        //         'name' => $this->name,
-        //         'image' => url('storage/product/' . $this->image),
-        //         'price' => round($originalPrice, 2),
-        //         'discount_price' => 0,
-        //         'addons' => AddOnResource::collection($this->getAddOns()),
-        //     ];
-        // }
-        
+// $originalPrice = $this->price;
+// $discount = $this->discount ?? 0;
+// $discountType = $this->discount_type ?? 'percent'; // assumes 'percent' or 'amount'
+
+// if ($discount > 0) {
+//     if ($discountType === 'percent') {
+//         $discountedPrice = $originalPrice - ($originalPrice * $discount / 100);
+//     } else { // fixed amount
+//         $discountedPrice = $originalPrice - $discount;
+//     }
+
+//     // Ensure price doesn't go negative
+//     $discountedPrice = max(0, $discountedPrice);
+
+//     $data = [
+//         'id' => $this->id,
+//         'name' => $this->name,
+//         'image' => url('storage/product/' . $this->image),
+//         'price' => round($discountedPrice, 2),
+//         'discount_price' => round($originalPrice, 2),
+//         'addons' => AddOnResource::collection($this->getAddOns()),
+//     ];
+// } else {
+//     // No discount
+//     $data = [
+//         'id' => $this->id,
+//         'name' => $this->name,
+//         'image' => url('storage/product/' . $this->image),
+//         'price' => round($originalPrice, 2),
+//         'discount_price' => 0,
+//         'addons' => AddOnResource::collection($this->getAddOns()),
+//     ];
+// }
+
 //         $id = $request->input('id');
 //         $food = Food::findOrFail($id);
 
 
 //         $data['variations'] = $food->variations->map(function ($variation, $index) {
 //             $heading = ucfirst($variation->name); // "Size", "Size 2", etc.
-    
+
 //             $options = $variation->variationOptions->map(function ($option) {
 //                 return [
 //                     'size' => $option->option_name,
@@ -150,18 +150,18 @@ if ($this->variationOptions && $this->variationOptions->count() > 0) {
 //                     'discount_price' => $option->discount_price ?? 0,
 //                 ];
 //             });
-    
+
 //             return [
 //                 'heading' => $heading,
 //                 'data' => $options,
 //             ];
 //         });
-    
+
 //         return response()->json($data);
-    
-        
-        
-        
+
+
+
+
 //     }
 
 //     private function getAddOns()
@@ -171,7 +171,7 @@ if ($this->variationOptions && $this->variationOptions->count() > 0) {
 //             if (is_array($ids)) {
 //                 // Ensure integer and unique IDs
 //                 $uniqueIds = array_unique(array_map('intval', $ids));
-    
+
 //                 // Fetch only once, then ensure unique collection by ID
 //                 return \App\Models\AddOn::whereIn('id', $uniqueIds)
 //                     ->get()
@@ -179,7 +179,7 @@ if ($this->variationOptions && $this->variationOptions->count() > 0) {
 //                     ->values(); // reindex to avoid gaps
 //             }
 //         }
-    
+
 //         return collect();
 //     }
 // }

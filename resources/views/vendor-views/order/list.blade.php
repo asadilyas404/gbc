b95525122d1b7c8187d10176811e1b4447dcba7d
asadilyas404 <asadilyas404@gmail.com>
2025-09-09 12:19:53 +0500

    asd

--- resources/views/vendor-views/order/list.blade.php
+++ resources/views/vendor-views/order/list.blade.php
@@ -12,7 +12,8 @@
 
     <style>
         .col-auto {
-            padding: 0.25rem !important;
+            padding-left: 0.25rem !important;
+            padding-right: 0.25rem !important;
         }
         .order-card {
             transition: all 0.2s ease-in-out;
@@ -311,7 +312,7 @@
                 </div>
 
                 <!-- Search and Actions Row -->
-                <div class="row w-70 align-items-center">
+                <div class="row w-75 align-items-center">
                     <!-- Search Bar -->
                     <div class="col-12">
                         <div class="search--button-wrapper justify-content-end max-sm-flex-100">

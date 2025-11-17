4c3b9182509e2389f90faae513f33ca0fc3df19c
asadilyas404 <asadilyas404@gmail.com>
2025-11-17 14:12:11 +0500

    asd

--- app/Http/Controllers/Vendor/BusinessSettingsController.php
+++ app/Http/Controllers/Vendor/BusinessSettingsController.php
@@ -303,21 +303,21 @@ class BusinessSettingsController extends Controller
 
     public function syncUsers()
     {
-        SyncEmployeesJob::dispatch();
+        SyncEmployeesJob::dispatchSync();
         Toastr::success('Users sync successfully!');
         return back();
     }
 
     public function syncCustomers()
     {
-        SyncCustomersJob::dispatch();
+        SyncCustomersJob::dispatchSync();
         Toastr::success('Customers sync successfully!');
         return back();
     }
 
     public function syncBranchesRestaurants()
     {
-        SyncBranchesRestaurantsJob::dispatch();
+        SyncBranchesRestaurantsJob::dispatchSync();
         Toastr::success('Branches & Restaurants sync successfully!');
         return back();
     }
--- app/Http/Controllers/Vendor/FoodController.php
+++ app/Http/Controllers/Vendor/FoodController.php
@@ -1326,7 +1326,7 @@ class FoodController extends Controller
 
     public function sync()
     {
-        SyncFoodJob::dispatch();
+        SyncFoodJob::dispatchSync();
         Toastr::success('Foods Sync completed!');
         return back();
     }

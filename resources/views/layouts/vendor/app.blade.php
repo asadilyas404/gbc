8cc1506081752e25626f9fbe72e6e55e5b18c887
Zaryab Akhtar <zaryabakhtar9@gmail.com>
2026-01-26 11:48:56 +0400

    Update app.blade.php

--- resources/views/layouts/vendor/app.blade.php
+++ resources/views/layouts/vendor/app.blade.php
@@ -859,8 +859,8 @@ function escapeHtml(str) {
 }
 
 // On page load + every 5 minutes
-posCheckForUpdates();
-setInterval(posCheckForUpdates, 1 * 60 * 1000);
+// posCheckForUpdates();
+// setInterval(posCheckForUpdates, 1 * 60 * 1000);
 </script>
 
 
